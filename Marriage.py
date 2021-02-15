1eae31db4b90cf8e4b6d086ee14ce8650d0a2f3a
Mr Xoxo <32399569+AX0123SR@users.noreply.github.com>
2021-02-15 12:57:22 +0530

    Add files via upload

---
+++ Marriage.py
@@ -0,0 +1,35 @@
+import time
+from turtle import *
+from PIL import Image
+import turtle
+
+pen = turtle.Turtle()
+
+color("red")
+begin_fill()
+left(40)
+forward(150)
+
+circle(90,180)
+left(280)
+circle(90,180)
+forward(150)
+end_fill()
+pen.up()
+
+# Move turtle to a given position
+pen.setpos(-120,120)
+pen.write("Happy marriage anniversary \n", font=("Helvetica", 14, "bold"))
+pen.write("           Mom and Dad ", font=("Helvetica", 14, "bold"))
+time.sleep(6)
+img = Image.open("Pic1.jpg")
+print(img.size)
+img.show()
+
+img1 = Image.open("Pic2.jpg")
+print(img1.size)
+img.show()
+
+time.sleep(2)
+#img.close()
+done
